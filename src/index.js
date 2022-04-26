81de2e089f027f019f15834f79619b11d731bee8
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2022-04-26 22:46:28 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './login'
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+const axios = require('axios').default;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    //<App />
+    <Login />
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/login.js
@@ -0,0 +1,67 @@
+import "./login.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+function Login() {
+  const [password, setPassword] = useState("");
+  const [User, setUser] = useState("");
+  const [passwordError, setpasswordError] = useState("");
+  const handleValidation = (event) => {
+    let formIsValid = true;
+    return formIsValid;
+  };
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    handleValidation();
+  };
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="text-kmutnb-name">
+          <h2> ระบบจัดการสารสนเทศ มจพ</h2>
+        </div>
+        <div className="row d-flex justify-content-end">
+          <div className="col-md-4 form-login">
+            <h2>ลงชื่อเข้าใช้ด้วยบัญชี ICIT</h2>
+            <form id="loginform" onSubmit={loginSubmit}>
+              <div className="form-group">
+                <label>Student ID</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="text"
+                  name="text"
+                  placeholder="ไม่ต้องใส่ @kmutnb.ac.th"
+                  onChange={(event) => setUser(event.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <small id="passworderror" className="text-danger form-text">
+                  {passwordError}
+                </small>
+              </div>
+              <button type="submit" className="btn btn-primary ">
+                Login
+              </button>
+            </form>
+          </div>
+          
+        </div>
+        <footer className="footer footer-text">
+       Deverlopment by React
+      </footer>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
